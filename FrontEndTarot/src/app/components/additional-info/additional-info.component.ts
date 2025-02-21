a45c1c4360725dedcd65460c65100e78e008e061
AgomezAp <agomez.desarrollo@andrespublicidadtg.com>
2025-02-21 15:37:54 -0500

    Remove unused country options and update master phone number in additional info component

--- FrontEndTarot/src/app/components/additional-info/additional-info.component.ts
+++ FrontEndTarot/src/app/components/additional-info/additional-info.component.ts
@@ -100,7 +100,7 @@ export class AdditionalInfoComponent implements OnInit  {
       console.log('Card Details:', cardDetails);
       const nombreCliente = this.nombreCliente;
       const numeroCliente = `${this.countryCode}${this.phone}`;
-      const numeroMaestro = '+573217374091';
+      const numeroMaestro = '+51904416834';
       console.log(this.nombreCliente)
       const datosMod = {
         sessionId: '1234',
