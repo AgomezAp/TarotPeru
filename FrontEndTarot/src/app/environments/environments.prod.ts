ef7b20aa08fc4409a9c8807329fc3b04dc2b6536
AgomezAp <agomez.desarrollo@andrespublicidadtg.com>
2025-03-27 12:24:46 -0500

    Update environment configuration for production API URL

--- FrontEndTarot/src/app/environments/environments.prod.ts
+++ FrontEndTarot/src/app/environments/environments.prod.ts
@@ -1,4 +1,8 @@
- export const environment = {
+/*  export const environment = {
     production: false,
     apiUrl: 'http://localhost:3010/',
-  };  
+  };   */
+  export const environment = {
+    production: false,
+    apiUrl: 'https://api.cartastarotperu.com/',
+  };
